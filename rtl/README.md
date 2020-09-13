9260c430598a58ff2c752a8a221350f491aa2750
TheOneKevin <kevindai02@outlook.com>
2020-09-12 23:18:31 -0400

    Added Verilator support

---
+++ rtl/README.md
@@ -0,0 +1,4 @@
+To build the main Verilator testbench, run at the root directory:
+```
+make -f machine/verilator/Makefile
+```
--- rtl/machine/tb.v
+++ rtl/machine/tb.v
@@ -7,10 +7,24 @@
 `include "machine/AS6C1008.v"
 `include "main.v"
 
-module tb();
+module tb(
+`ifndef SIMULATION
+    input reg clk
+`endif
+);
     parameter PERIOD = 62;
+`ifdef SIMULATION
+    parameter himem = "../machine/hi.mem";
+    parameter lomem = "../machine/lo.mem";
+`else
+    parameter himem = "machine/hi.mem";
+    parameter lomem = "machine/lo.mem";
+`endif
+    
+`ifdef SIMULATION
     reg clk;
     always #(PERIOD/2) clk=~clk;
+`endif
 
     wire [7:0] ad1;
     wire [7:0] ad2;
@@ -28,12 +42,13 @@ module tb();
     wire [7:0] data_hi;
 
     initial begin
-        clk = 0;
+`ifdef SIMULATION
+        clk = 0;        
         $dumpfile("tb.vcd");
         $dumpvars(0, u0, u1, u2, u3, u4, u5, u6, u7, u8, ad1, ad2, ad3, data_hi, data_lo);
-
         #(PERIOD*100)
         $finish;
+`endif
     end
 
     CPU u0(clk,,, io1, io2, io3, io4, io5, io6, io7, io8, io9, io10, io11, io12, io13, io14, io15, io16,
@@ -44,6 +59,6 @@ module tb();
     SN74F138    #(0)    u4(io18, io19, io20, 1'b0, 1'b0, pio, Y);
     SN74LVC245  #(0)    u5(we, oe, data_hi, { io1, io2, io3, io4, io5, io6, io7, io8 });
     SN74LVC245  #(0)    u6(we, oe, data_lo, { io9, io10, io11, io12, io13, io14, io15, io16 });
-    AS6C1008    #(0,0,"../machine/hi.mem")  u7({ 9'b0, ad2 }, data_hi, Y[0:0], 1'b1, we, 1'b0);
-    AS6C1008    #(0,0,"../machine/lo.mem")  u8({ 9'b0, ad2 }, data_lo, Y[0:0], 1'b1, we, 1'b0);
+    AS6C1008    #(0,0,himem)  u7({ 9'b0, ad2 }, data_hi, Y[0:0], 1'b1, we, 1'b0);
+    AS6C1008    #(0,0,lomem)  u8({ 9'b0, ad2 }, data_lo, Y[0:0], 1'b1, we, 1'b0);
 endmodule
